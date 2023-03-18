c127af47a5117d33ee2e652e37981a872aab521b
tyryotoy <tyryotoy@gmail.com>
2023-03-19 02:21:30 +0900

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from 'astro/config';
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  // site: "",
+  integrations: [
+    tailwind(),
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date(),
+      entryLimit: 10000,
+    }),
+    robotsTxt(),
+  ],
+});
