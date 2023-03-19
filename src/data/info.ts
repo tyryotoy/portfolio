5f96a28080bf418cd17fa0cfc52a98d1f47712c4
tyryotoy <tyryotoy@gmail.com>
2023-03-20 00:21:23 +0900

    Replacement of personal information

--- src/data/info.ts
+++ src/data/info.ts
@@ -2,93 +2,82 @@ export const info = {
   baseUrl: "https://astro-portfolio-uzair.vercel.app",
   name: "TYRYOTOY",
   jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
-    I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-    I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-    I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-    I have successfully completed several web projects from scratch.
-    Yet, I learn continuously and never stop exploring new technologies.
-    So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-    firebase and React Native with time.
+  about: `フロントエンド、バックエンド、インフラなどを扱うフルスタックエンジニア。
+    高専で情報工学、大学でセキュリティ情報学を学んだのち、プログラミング学習サービスを展開するベンチャー企業にて製品開発部として社内業務の効率化とデータ分析、マッチングシステムの設計・開発・保守・運用、Webサービスの開発・保守・運用に従事。
+    その後システム開発部にて顧客管理システムの開発責任者と引き続き社内業務の効率化・データ分析基盤の開発に従事。
+    諸事情によりSES会社へ転職し、遊技機情報サイトの開発とECサイトのプラットフォームの開発、スマートフォンアプリのプロトタイプ開発に従事。
+    自社プロダクトの開発、働き方の自由度を求め音声創作ソフトウェアサービスを展開する企業へ転職し、Web周りの全て(フロントエンド・バックエンド・インフラなど)を0->1にて開発。
+    現在はWeb周りの全ての保守・運用を行いながら、新規プロダクトの企画・設計などを行なっております。
   `,
 
   experience: [
     {
-      name: "InterNative Labs, LLC",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "Apr 2022",
-      endDate: "Present",
+      name: "株式会社T (フルリモート)",
+      location: "",
+      startDate: "2021年6月",
+      endDate: "現在",
       description: [
-        "- Developing screens and UI components for the web application using React and Tailwind.",
-        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+        "音声創作ソフトウェアサービスの開発",
       ],
     },
     {
-      name: "Coupon Approve (Remote)",
-      location: "Amsterdam, North Holland, Netherlands",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
+      name: "株式会社C (一部リモート)",
+      location: "",
+      startDate: "2020年3月",
+      endDate: "2021年5月",
       description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
+        "自社コーポレートサイトの開発",
+        "遊技機情報サイトの開発",
+        "ライブストリーミングサイトの開発",
+        "ECサイトのプラットフォーム開発",
       ],
     },
     {
-      name: "Interns Pakistan",
-      location: "Pakistan",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
+      name: "株式会社S (一部リモート)",
+      location: "",
+      startDate: "2018年5月",
+      endDate: "2020年2月",
       description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+        "プログラミングレッスン講師",
+        "プログラミングに関してのQAサイト開発",
+        "生徒と講師のマッチングシステム開発",
+        "顧客管理システム (CRM) の保守・運用",
+        "経費精算自動化のシステム開発",
       ],
     },
   ],
 
   education: [
     {
-      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2019",
-      endDate: "2022",
-      description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
-      ],
-    },
-    {
-      name: "Govt. Degree Boys College Buffer Zone",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2018",
-      endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
-    },
-    {
-      name: "Ghazi Foundation Schools & Colleges",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2016",
+      name: "D 大学 (3年次編入 -> 中退)",
+      location: "",
+      startDate: "2017",
       endDate: "2018",
-      description: ["Matriculation,  Computer Science"],
+      description: [""],
+    },
+    {
+      name: "S 高専",
+      location: "",
+      startDate: "2012",
+      endDate: "2017",
+      description: [""],
     },
   ],
 
   socialMedia: {
-    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
-    twitter: "https://twitter.com/Uzzii_21",
-    github: "https://github.com/uzzii-21",
-    email: "uzairahmed@74372gmail.com",
-    linkedin: "https://www.linkedin.com/in/uzzii21/",
+    // facebook: "",
+    // twitter: "",
+    github: "https://github.com/tyryotoy",
+    email: "tyryotoy@gmail.com",
+    // linkedin: "",
   },
 
   projects: [
     {
-      title: "Astro Portfolio",
+      title: "Portfolio",
       isFeatured: true,
-      thumbnail: "/assets/images/astro-portfolio.png",
-      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      thumbnail: "/assets/images/portfolio.png",
+      githubUrl: "https://github.com/tyryotoy/portfolio",
       liveUrl: "https://astro-portfolio-uzair.vercel.app/",
     },
   ],
